35f4e4c1d8447344b1f153d6580119e6024b7270
HeJimenez00 <hejimenez00@gmail.com>
2025-01-23 18:46:09 -0600

    Fix responsive navbar

---
+++ src/components/MenuToggle.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+export const MenuToggle = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <button
+      className="navbar-icon md:hidden hover:bg-black-300 p-[6px] rounded-md "
+      onClick={toggleMenu}
+    >
+      {!isOpen ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-ellipsis-vertical"
+        >
+          <circle cx="12" cy="12" r="1" />
+          <circle cx="12" cy="5" r="1" />
+          <circle cx="12" cy="19" r="1" />
+        </svg>
+      ) : (
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-x"
+        >
+          <path d="M18 6 6 18"></path>
+          <path d="m6 6 12 12"></path>
+        </svg>
+      )}
+    </button>
+  );
+};
