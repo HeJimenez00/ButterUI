82cdee18805b5264998922f9a2316d4c4be425f3
HeJimenez00 <hejimenez00@gmail.com>
2025-03-25 00:48:28 -0600

    fix: correct orthographic camera and zoom controls settings

--- src/components/experience/Camera.jsx
+++ src/components/experience/Camera.jsx
@@ -1,13 +1,15 @@
 import { OrthographicCamera } from "@react-three/drei";
 
 export const Camera = () => {
+  const scaleValue = 0.4;
+
   return (
     <OrthographicCamera
-      args={[-5, 5, 5, -5]}
       makeDefault
+      args={[-5, 5, 5, -5]}
       position={[10, 20, 20]}
-      scale={[1.1, 1, 1]}
-      zoom={50}
+      scale={[scaleValue + 0.04, scaleValue, scaleValue]}
+      zoom={20}
     />
   );
 };
--- src/components/experience/Controls.jsx
+++ src/components/experience/Controls.jsx
@@ -1,5 +1,22 @@
 import { OrbitControls } from "@react-three/drei";
 
 export const Controls = () => {
-  return <OrbitControls />;
+  // const controls = useControls("Limitaciones en los controles", {
+  //   autoRotate: { value: false },
+  //   enablePan: { value: false },
+  //   zoomSpeed: { min: 0, max: 1, value: 0.1, step: 0.01 },
+  //   minZoom: { min: 0.1, max: 18, value: 18, step: 0.1 },
+  //   maxZoom: { min: 0.1, max: 20, value: 20, step: 0.1 },
+  // });
+
+  return (
+    <OrbitControls
+      autoRotate={false}
+      enablePan={false}
+      minZoom={18}
+      maxZoom={20}
+      minPolarAngle={Math.PI / 3.7}
+      maxPolarAngle={Math.PI / 3.7}
+    />
+  );
 };
